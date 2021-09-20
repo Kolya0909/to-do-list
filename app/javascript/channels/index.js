990083bd217ff1ab14d634a7199618b0cc6da66b
Kolya <bond410@ukr.net>
2021-09-20 19:48:35 +0300

    create custom  user registration

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,10 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+window.onload = () =>{
+    const signup = document.querySelector('.signup__btn')
+
+    signup.addEventListener('click', ()=>{
+        document.querySelector('.modal__signup').classList.toggle('modal__signup-active')
+    })
+}
