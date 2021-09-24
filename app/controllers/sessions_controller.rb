8b38d5f9ab533d61ee9eb28d2d2d8e9d62314f1c
Kolya <bond410@ukr.net>
2021-09-24 23:44:15 +0300

    complete user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email:params[:email]
     if user&.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect_to root_path
     else
       flash[:warning] = "Invalid password or email!"
@@ -15,7 +16,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
+    session.delete(:user_id)
+    redirect_to root_path
   end
 
 end
